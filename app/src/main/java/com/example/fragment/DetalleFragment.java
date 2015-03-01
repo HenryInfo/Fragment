2171d8767726460d84a69c80b818821d401f6fb0
HenryInfo <hbravos@unc.edu.pe>
2015-03-01 05:49:58 +0100

    Update DetalleFragment.java

--- app/src/main/java/com/example/fragment/DetalleFragment.java
+++ app/src/main/java/com/example/fragment/DetalleFragment.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.caxas.fragmentbookbiblio.R;
 
 /**
  * Created by henryyerrybravosanchez on 2/25/15.
