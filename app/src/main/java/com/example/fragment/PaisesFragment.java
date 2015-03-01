3cc40fda298532d726893303437d760e40774dae
HenryInfo <hbravos@unc.edu.pe>
2015-03-01 05:50:43 +0100

    Update PaisesFragment.java

--- app/src/main/java/com/example/fragment/PaisesFragment.java
+++ app/src/main/java/com/example/fragment/PaisesFragment.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import com.caxas.fragmentbookbiblio.R;
 
 /**
  * Created by henryyerrybravosanchez on 2/25/15.
